1b2a5a58a61060482fc6f7989ed6e491c5119f53
nguyen van thai <nguyenvanthailtv@gmail.com>
2023-11-12 23:28:02 +0700

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function addUser() {
+        $user = [
+            'name' => 'abc',
+            'email' => 'abc@example.com',
+            'password' => '123456789',
+        ];
+        User::create($user);
+        return $user;
+    }
 }
